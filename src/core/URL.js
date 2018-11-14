8a19a92765261fe9f409bfa4d4164ed73b9b63b9
bernie <mail@javaner.de>
2018-11-14 09:22:21 +0100

    add: URL.currentScriptURL, version 0.0.8

--- src/core/URL.js
+++ src/core/URL.js
@@ -1,14 +1,24 @@
 /**
  * Created by Hessberger on 10.10.2014.
  */
-define(["lodash"], function (_) {
-    var ret = {};
+define(["lodash", "jquery"], function ( _, $ ) {
+    var URL = {};
 
     _.each(_.compact( _.map( window.location.search.slice(1).split('&'), function( item ){
         if( item )return item.split( '=' );
     })) , function( p ){
-        ret[p[0]] = p[1];
+        URL[p[0]] = p[1];
     });
+    
+    URL.currentScriptURL = function(){
+        let $el = $("script[data-main]")[0];
+        let main = $el.attr("data-main");
+        let ret = main.substring( 0, main.indexOf( "js/" ));
+        
+        if ( ret === "/" ) { ret = ""; }
+        
+        return ret;
+    };
 
-    return ret;
+    return URL;
 });
