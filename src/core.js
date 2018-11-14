7de63f893faeb6a2c7ed2a734373b9bcbed8fbf1
bernie <mail@javaner.de>
2018-11-14 16:45:35 +0100

    add: es3,babel, Events; version 0.0.11

--- src/core.js
+++ src/core.js
@@ -8,14 +8,14 @@
  * @returns {packL#5.packAnonym$1}
  */
 
-define(["async", "json", "i18n", "base64", "vendor/base64", 
-    "less", "lessc", "libs/normalize", 
+define(["async", "json", "i18n", "es6", "base64", "vendor/base64", 
+    "less", "lessc", "libs/normalize", "events", 
     "url", "three", "lodash", "jquery", "backbone", 
     "cmd", "globals", "tween", "dat-gui", "plugin", "stats", 
     "ThreeCSG", "vendor/require/normalize", 
     "vendor/polyfill/CustomEvent", "vendor/polyfill/Function"
 ], 
-function( async, json, i18n, base64, utilbase64, less, lessc, normalize, URL, THREE, _, $, Backbone, CMD, GLOBALS, TWEEN, dat, Plugin, Stats ) {
+function( async, json, i18n, es6, base64, utilbase64, less, lessc, normalize, Events, URL, THREE, _, $, Backbone, CMD, GLOBALS, TWEEN, dat, Plugin, Stats ) {
     return {
         "_"         : _,
         "$"         : $,
@@ -23,6 +23,7 @@ function( async, json, i18n, base64, utilbase64, less, lessc, normalize, URL, TH
         "Backbone"  : Backbone,
         "CMD"       : CMD,
         "dat"       : dat,
+        "Events"    : Events,
         "GLOBALS"   : GLOBALS,
         "Plugin"    : Plugin,
         "THREE"     : THREE,
@@ -33,7 +34,8 @@ function( async, json, i18n, base64, utilbase64, less, lessc, normalize, URL, TH
         "requirejs" : {
             "json" : json,
             "less" : less,
-            "i18n" : i18n
+            "i18n" : i18n,
+            "es6"  : es6
         }
      };
 });
