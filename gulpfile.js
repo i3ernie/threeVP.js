7865336ba84c483aae9667d2bc9ee1ac0232c440
bernie <mail@javaner.de>
2020-04-08 18:54:48 +0200

    update: config file for core, add: config file for OKP

--- gulpfile.js
+++ gulpfile.js
@@ -11,11 +11,16 @@ var plugins = require('gulp-load-plugins')(); // Load all gulp plugins
 var _ = require('lodash');
 var fs = require('fs');
 const rollup  = require('rollup');
-const gulpRollup  = require('gulp-better-rollup');
 const resolve =require('rollup-plugin-node-resolve');
 const alias  = require('rollup-plugin-alias');
 const sourcemaps = require('gulp-sourcemaps');
 const cjs = require("rollup-plugin-cjs-es");
+const rollup_amd = require( 'rollup-plugin-amd' );
+const rollup_legacy = require( 'rollup-plugin-legacy');
+
+const task_onpla = require('./build/build_onpla');
+const task_core = require('./build/build_core');
+const task_viewport = require('./build/build_viewport');
 
 var pkg = require('./package.json');
 var dirs = pkg.directories;
@@ -43,6 +48,8 @@ gulp.task('default', ( done ) => {
     // place code for your default task here
 });
 
+gulp.task('task_onpla', task_onpla);
+
 gulp.task('init', ( done ) => {
     
     var fnc = function( src, dest, req, name, mod )
@@ -61,7 +68,7 @@ gulp.task('init', ( done ) => {
     
     var modules = require("./modules.json");
     
-    _.each(modules, ( el ) =>{
+    _.each(modules, ( el ) =>{ 
         fnc(el.src, el.dest, el.req , el.name, el.mod);
     });    
     done();
@@ -70,97 +77,79 @@ gulp.task('init', ( done ) => {
 
 gulp.task("build", ( done ) => {
     "use strict";
-    var ret = plugins.requirejs(
-        _.extend({}, requireconfig,
-            {
-                "name"      : "Viewport",
-                "exclude"   : ["three", "lodash", "backbone", "jquery", "cmd", "json", "text"],
-                "out"       : "Viewport.js",
-                "generateSourceMaps": true
-            })
-    ).on("error", console.log)
-    .pipe( gulp.dest( dirs.dist ) );
-
-    plugins.requirejs(
-        _.extend({}, requireconfig,
-            {
-                "name"      : "Viewport",
-                "exclude"   : ["three", "lodash", "backbone", "jquery", "cmd", "json", "text"],
-                "out"       : "Viewport.min.js"
-            })
-        ).on("error", console.log)
-    .pipe( plugins.uglify() )
-    .pipe( gulp.dest( dirs.dist ) );
+    task_viewport( ()=>{
+        task_core( done );
+    });
     
-    plugins.requirejs(
-        _.extend({}, requireconfig,
-            {
-                "name"      : "vendor/require/require",
-                "exclude"   : [],
-                "out"       : "core.js",
-                "include" : ["underscore", "core"]
-            })
-        ).on("error", console.log)
-    .pipe( gulp.dest( dirs.dist ) );
-    
-    plugins.requirejs(
-        _.extend({}, requireconfig,
-            {
-                "name"      : "vendor/require/require",
-                "exclude"   : [],
-                "out"       : "core.min.js",
-                "include" : ["underscore", "core"]
-            })
-        ).on("error", console.log)
-    .pipe( plugins.uglify() )
-    .pipe( gulp.dest( dirs.dist ) );
-
-    return ret;
 });
 
 gulp.task('packThreeVPModule', function( done ){
  
-    rollupBuild( {
-        input: 'src/threeVP.js',
+    return rollup.rollup({
+        input: 'src/threeVP.es6.js',
         plugins: [ 
             alias({
                 resolve: ['.jsx', '.js'], 
                 entries:[
                   {find:'underscore', replacement: './../../lodash-es/lodash.js'}, 
-                  {find:'jquery', replacement: './../../../src/vendor/jquery/jquery.es6.js'}
+                  {find:'jquery', replacement: 'node_modules/jquery/src/jquery.js'}
                 ]
             }),
-            cjs({
-                nested: false
+            rollup_amd(),
+            rollup_legacy({
+                "node_modules/url/url.js" : 'url'
             }),
-            resolve({
-                preferBuiltins: false
-            }) 
+            resolve()
+            
         ]
-    }, {
-        file: 'dist/threeVP.module.js',
-        exports : 'named',
-        format: 'es'
-    }, done );
+    })
+    .then(( bundle ) => {
+        return bundle.write({
+            file:"dist/threeVP.es.js",
+            format: 'es', 
+            name: 'threeVP',
+            exports: 'named'
+    })
+        
+    });
 });
+
 
 gulp.task('packThreeVPUMD', function( done ){
  
-    rollupBuild( {
-        input: 'src/threeVP.js',
-        plugins: [ resolve() ]
-    }, {
-        file: 'dist/threeVP.umd.js',
-        exports : 'named',
-        name: 'threeVP',
-        format: 'umd'
-    }, done );
+    return rollup.rollup({
+        input: 'src/threeVP.es6.js',
+        plugins: [ 
+            alias({
+                resolve: ['.jsx', '.js'], 
+                entries:[
+                  {find:'underscore', replacement: './../../lodash-es/lodash.js'},
+                  {find:'jquery', replacement: 'node_modules/jquery/src/jquery.js'}
+                ]
+            }),
+            rollup_legacy({
+                "node_modules/url/url.js" : 'url',
+                "node_modules/tween.js/src/Tween" : "Tween"
+            }),
+            rollup_amd(),
+            resolve() 
+        ]
+    })
+    .then(( bundle ) => {
+        return bundle.write({
+            file:"dist/threeVP.umd.js",
+            format: 'umd', 
+            name: 'threeVP',
+            exports: 'named'
+    })
+        
+    });
 });
 
 gulp.task('packThreeVPAMD', function( done ){
  
     rollupBuild( {
-        input: 'src/threeVP.js',
+        input: 'src/threeVP.es6.js',
         plugins: [ resolve() ]
     }, {
         file: 'dist/threeVP.amd.js',
@@ -172,14 +161,32 @@ gulp.task('packThreeVPAMD', function( done ){
 
 gulp.task('packAsyncModule', function( done ){
  
-    rollupBuild( {
+    return rollup.rollup({
         input: 'src/libs/async.es.js',
-        plugins: [ resolve() ]
-    }, {
-        file: 'src/libs/async.pack.es.js',
-        exports : 'named',
-        format: 'es'
-    }, done );
+        plugins: [ 
+            alias({
+                resolve: ['.jsx', '.js'], 
+                entries:[
+                  {find:'underscore', replacement: './../../lodash-es/lodash.js'}, 
+                  {find:'jquery', replacement: './../../../node_modules/jquery/src/jquery.js'}
+                ]
+            }),
+            rollup_legacy({
+                "url/url" : 'url'
+            }),
+            rollup_amd(),
+            resolve() 
+        ]
+    })
+    .then(( bundle ) => {
+        return bundle.write({
+            file:"dist/async.es.js",
+            format: 'es', 
+            name: 'async',
+            exports: 'named'
+    });
+        
+    });
 });
 
 gulp.task('bundleAsync', function( done ) {
